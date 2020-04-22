28a8afd1cea10d1dc6e18d1ced645bcf1887212f
romar <romarioprime@mail.ru>
2020-04-23 02:47:06 +0300

    Добавлена проверка на уход за поля при ускорении (частично работает, есть исключения), добавлена полностью рабочая проверка на спаун кругов без пересечений друг друга (появилось соответственное ограничение на кол-во кругов на экране), оставлена закоменченная логика при сравнении с 0, неоюходимо доработать с использованием формул диагонали описанного квадрата и сравнением растояний между координатами с радиусом (проблема в точности flout)

--- app/src/main/java/com/example/myapplication/MyView3.java
+++ app/src/main/java/com/example/myapplication/MyView3.java
@@ -22,7 +22,9 @@ public class MyView3 extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         int R = 100;
-        int B = 150;
+        int B = 165;
+//        int P = 0;
+
         if (!start) {
             for (int i = 0; i < N; i++){
                 x[i] = (float)(Math.random() * getWidth());
@@ -33,23 +35,37 @@ public class MyView3 extends View {
                 if ((x[i] - R < 0 ||  (x[i] + R) >= canvas.getWidth()) || (y[i] - R < 0 || (y[i] + R) >= canvas.getHeight())) {
                     i--;
                 }
+                if (i > 0) {
+                    for (int j = 1; j < i+1; j++) {
+                        if ((Math.abs(Math.abs(x[i]) - Math.abs(x[i-j])) <= R*2) && ((Math.abs(Math.abs(y[i]) - Math.abs(y[i-j])) <= R*2))) {
+                            i--;
+                            break;
+                        }
+                    }
+                }
             }
 
             start = true;
         }
         super.onDraw(canvas);
 
-
         int []col = {Color.GREEN, Color.YELLOW, Color.BLUE, Color.BLACK, Color.RED, Color.GRAY, Color.DKGRAY, Color.CYAN, Color.MAGENTA, Color.LTGRAY};
-
 
         for (int i = 0; i < N; i++) {
             paint.setColor(col[i%10]);
-            canvas.drawCircle(x[i], y[i], 100, paint);
+            canvas.drawCircle(x[i], y[i], R, paint);
         }
 
         do {
             for (int i = 0; i < N; i++) {
+
+                if (x[i]-vx[i] < 0 || x[i]+vx[i] >= canvas.getWidth()) {
+                    vx[i] /=4;
+                }
+                if (y[i]-vy[i] < 0 || y[i]+vy[i] >= canvas.getHeight()) {
+                    vy[i] /=4;
+                }
+
                 x[i] += vx[i];
                 y[i] += vy[i];
 
@@ -59,11 +75,40 @@ public class MyView3 extends View {
                 int xl = 0;
                 int yu = 0;
                 int yd = 0;
+//                float XR = 0;
+//                float YR = 0;
 
                 for (int ip = 0; ip < N; ip++) {
                     if (ip == i) {
                         continue;
                     }
+
+//для логики сравнения с 0
+//                    xr = ((int) ((Math.abs(x[i])+R) - (Math.abs(x[ip])-R)));
+//                    xl = ((int) ((Math.abs(x[i])-R) - (Math.abs(x[ip])+R)));
+//                    yu = ((int) ((Math.abs(y[i])+R) - (Math.abs(y[ip])-R)));
+//                    yd = ((int) ((Math.abs(y[i])-R) - (Math.abs(y[ip])+R)));
+//
+//                    XR = Math.abs(Math.abs(x[i])-Math.abs(x[ip]))/2;
+//                    YR = Math.abs(Math.abs(y[i])-Math.abs(y[ip]))/2;
+//                    int D = (int)(Math.sqrt(2)*R);
+//логика сравнения с 0 (не работает (доделать))
+//                    if ((xr == P || xl == P) && (yu + yd == P) && (XR == R)) {
+//                        vx[i] = vx[i] * (-1);
+//                    }
+//                    if ((yu == P || yd == P) && (xr + xl == P) && (YR == R)) {
+//                        vy[i] = vy[i] * (-1);
+//                    }
+//                    if ((xr*xl - yu*yd == P) && ((D == XR*2) && (D == YR*2))) {
+//                        vx[i] = vx[i] * (-1);
+//                        vy[i] = vy[i] * (-1);
+//                    }
+//                    if ((xr == P || xl == P) && (((xr + (-yu+yd)) == P) || ((xl + (-yu+yd)) == P)) && (XR == R)) {
+//                        vx[i] = vx[i] * (-1) ;
+//                    }
+//                    if ((yu == P || yd == P) && (((yu + (-xr+xl)) == P) || ((yd + (-xr+xl)) == P)) && (YR == R)) {
+//                        vy[i] = vy[i] * (-1) ;
+//                    }
 
                     xr = Math.abs((int) (Math.abs(x[i])+R - Math.abs(x[ip])-R));
                     xl = Math.abs((int) (Math.abs(x[i])-R - Math.abs(x[ip])+R));
@@ -71,8 +116,8 @@ public class MyView3 extends View {
                     yd = Math.abs((int) (Math.abs(y[i])-R - Math.abs(y[ip])+R));
 
                     if ((xr <= B || xl <= B) && (yu <= B || yd <= B)) {
-                        vx[i] = vx[i] * (-1);
-                        vy[i] = vy[i] * (-1);
+                        vx[i] = vx[i] * (-1) - vy[i]/4;
+                        vy[i] = vy[i] * (-1) - vx[i]/4;
                     }
                 }
 
@@ -82,10 +127,7 @@ public class MyView3 extends View {
                 else if (y[i] - R < 0 || (y[i] + R) >= canvas.getHeight()) {
                     vy[i] = vy[i] * (-1);
                 }
-
             }
         } while (!start);
-
     }
-
 }
